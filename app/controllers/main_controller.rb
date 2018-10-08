2da58228fde701e9ec39b9e795396352fd7f126c
Matthew Jeng <mjeng@berkeley.edu>
2018-10-07 18:34:48 -0700

    Finished lab portion of hw

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,20 @@ class MainController < ApplicationController
 
   def index
     # Uncomment and pass a parameter to the get function
-    # @w = WeatherService.get()
+    @city = params[:city]
+    @w = WeatherService.get(@city)
+    if not @w.nil?
+        @temp = @w[:temperature] - 273
+        @description = @w[:description]
+    end
+
+    city = City.new(
+        name: "city",
+        description: "bllleeeh",
+        temperature: 1
+    )
+    city.save
+
   end
 
 end
